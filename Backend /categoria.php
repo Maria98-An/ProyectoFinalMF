6d5ebfe2f2bbd581b002c72da503e34961f50539
Maria98-An <yatmafe@gmail.com>
2021-01-12 03:53:30 -0400

    tablas

---
+++ Backend /categoria.php
@@ -0,0 +1,33 @@
+<?php
+//conectando a la base de datos de php 
+   $host="185.201.11.212";
+   $contraseña="B!uwj$]M";
+   $bd="u868164586_wallas";
+   $usuario="u868164586_wallas";
+
+   $cnx = new mysqli($host, $bd, $contraseña,$usuario);
+   $errorCnx = false;
+   if($cnx->connect_error){
+       echo "error";
+       $errorCnx = true;
+   }else {
+       echo "conexion correcta :) Maria Felix";
+   }
+   if($errorCnx == false){
+       //crear la tabla usuario
+       $tmp="create table categoria1DBMariaFelix(
+        id int(10) AUTO_INCREMENT,
+        nombre varchar(25),
+        fechaIngreso datetime,
+        tipo varchar(25),
+        PRIMARY KEY (id)
+        
+        )";
+        if($cnx->query($tmp)){
+            echo "<br> se creo la tabla ";
+            //crear usuario admin 
+           
+        }else {
+            echo "<br>no se creo la tabla";
+        }
+   }
