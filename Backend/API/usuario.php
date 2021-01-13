0201c26ba6a79fabc0310c91939a33043fb10418
Maria98-An <yatmafe@gmail.com>
2021-01-13 18:54:03 -0400

    switch

--- Backend/API/usuario.php
+++ Backend/API/usuario.php
@@ -1,6 +1,19 @@
 <?php
 
-     echo "metodo HTTP: ".$_SERVER['REQUEST_METHOD'] ;
-    
+     #echo "metodo HTTP: ".$_SERVER['REQUEST_METHOD'] ;
+     switch($_SERVER['REQUEST_METHOD']){
+        case 'POST':
+            echo "Guardar";
+        break;
+        case 'GET':
+            echo "obtener Usuario/os";
+        break;
+        case 'PUT':
+            echo "Actualizar";
+        break;
+        case 'DELETE':
+            echo "Eliminar";
+            break;
+    }
 
  ?>     
