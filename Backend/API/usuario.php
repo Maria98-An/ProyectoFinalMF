da909a7a0dca89325d18c5e70f3e388d2752449c
Maria98-An <yatmafe@gmail.com>
2021-01-13 19:31:00 -0400

    end

--- Backend/API/usuario.php
+++ Backend/API/usuario.php
@@ -3,12 +3,15 @@
      #echo "metodo HTTP: ".$_SERVER['REQUEST_METHOD'] ;
      switch($_SERVER['REQUEST_METHOD']){
         case 'POST':
-            echo "Guardar";
+            $_POST = json_decode(file_get_contents('php://input'),true);
+            echo "Guardar Usuario" . $_POST['nombre'];
         break;
         case 'GET':
-            echo "obtener Usuario/os";
+            echo "parametro GET: ".$_GET['id'];
+            //echo "obtener Usuario/os";
         break;
         case 'PUT':
+            $_PUT = json_decode(file_get_contents('php://input'),true);
             echo "Actualizar";
         break;
         case 'DELETE':
